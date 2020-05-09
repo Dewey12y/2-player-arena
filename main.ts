1a945846f4674c246ed6c47c151271d0558cdb58
Dewey12y <62912344+Dewey12y@users.noreply.github.com>
2020-05-08 20:17:55 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,412 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+`
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+c c c c c c f f f f c c c c c c 
+c c c c f f 2 2 2 2 f f c c c c 
+c c f f 2 2 2 2 2 2 2 2 f f c c 
+c c f 2 2 2 2 2 2 2 2 2 2 f c c 
+c f 2 2 2 2 2 2 2 2 2 2 2 2 f c 
+c f 2 2 2 2 2 2 2 2 2 2 2 2 f c 
+f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+f 2 2 2 2 2 2 1 1 2 2 2 2 2 2 f 
+f 2 2 2 2 2 2 1 1 2 2 2 2 2 2 f 
+f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+c f 2 2 2 2 2 2 2 2 2 2 2 2 f c 
+c f 2 2 2 2 2 2 2 2 2 2 2 2 f c 
+c c f 2 2 2 2 2 2 2 2 2 2 f c c 
+c c f f 2 2 2 2 2 2 2 2 f f c c 
+c c c c f f 2 2 2 2 f f c c c c 
+c c c c c c f f f f c c c c c c 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+c c c c c c f f f f c c c c c c 
+c c c c f f 8 8 8 8 f f c c c c 
+c c f f 8 8 8 8 8 8 8 8 f f c c 
+c c f 8 8 8 8 8 8 8 8 8 8 f c c 
+c f 8 8 8 8 8 8 8 8 8 8 8 8 f c 
+c f 8 8 8 8 8 8 8 8 8 8 8 8 f c 
+f 8 8 8 8 8 8 8 8 8 8 8 8 8 8 f 
+f 8 8 8 8 8 8 1 1 8 8 8 8 8 8 f 
+f 8 8 8 8 8 8 1 1 8 8 8 8 8 8 f 
+f 8 8 8 8 8 8 8 8 8 8 8 8 8 8 f 
+c f 8 8 8 8 8 8 8 8 8 8 8 8 f c 
+c f 8 8 8 8 8 8 8 8 8 8 8 8 f c 
+c c f 8 8 8 8 8 8 8 8 8 8 f c c 
+c c f f 8 8 8 8 8 8 8 8 f f c c 
+c c c c f f 8 8 8 8 f f c c c c 
+c c c c c c f f f f c c c c c c 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+c c c c c c f f f f c c c c c c 
+c c c c f f 2 2 2 2 f f c c c c 
+c c f f 2 2 2 2 2 2 2 2 f f c c 
+c c f 2 2 2 2 2 2 2 2 2 2 f c c 
+c f 2 8 6 2 2 2 2 2 2 8 6 2 f c 
+c f 2 8 8 2 2 8 6 2 2 8 8 2 f c 
+f 2 2 2 5 2 2 8 8 2 2 5 2 2 2 f 
+f 2 2 2 5 5 2 5 5 2 5 5 2 2 2 f 
+f 2 2 2 5 5 5 5 5 5 5 5 2 2 2 f 
+f 2 2 2 5 5 5 5 5 5 5 5 2 2 2 f 
+c f 2 2 2 2 2 2 2 2 2 2 2 2 f c 
+c f 2 2 2 2 2 2 2 2 2 2 2 2 f c 
+c c f 2 2 2 2 2 2 2 2 2 2 f c c 
+c c f f 2 2 2 2 2 2 2 2 f f c c 
+c c c c f f 2 2 2 2 f f c c c c 
+c c c c c c f f f f c c c c c c 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+c c c c c c f f f f c c c c c c 
+c c c c f f 8 8 8 8 f f c c c c 
+c c f f 8 8 8 8 8 8 8 8 f f c c 
+c c f 8 8 8 8 8 8 8 8 8 8 f c c 
+c f 8 2 3 8 8 8 8 8 8 2 3 8 f c 
+c f 8 2 2 8 8 2 3 8 8 2 2 8 f c 
+f 8 8 8 5 8 8 2 2 8 8 5 8 8 8 f 
+f 8 8 8 5 5 8 5 5 8 5 5 8 8 8 f 
+f 8 8 8 5 5 5 5 5 5 5 5 8 8 8 f 
+f 8 8 8 5 5 5 5 5 5 5 5 8 8 8 f 
+c f 8 8 8 8 8 8 8 8 8 8 8 8 f c 
+c f 8 8 8 8 8 8 8 8 8 8 8 8 f c 
+c c f 8 8 8 8 8 8 8 8 8 8 f c c 
+c c f f 8 8 8 8 8 8 8 8 f f c c 
+c c c c f f 8 8 8 8 f f c c c c 
+c c c c c c f f f f c c c c c c 
+`
+}
+scene.onOverlapTile(SpriteKind.Player, myTiles.tile1, function (sprite, location) {
+    tiles.placeOnTile(sprite, tiles.getTileLocation(Math.randomRange(6, 9), Math.randomRange(6, 9)))
+    music.pewPew.play()
+    if (p1 == sprite) {
+        info.player2.changeScoreBy(1)
+    } else {
+        info.changeScoreBy(1)
+    }
+    sprite.setVelocity(0, 0)
+    scene.cameraShake(4, 500)
+})
+function p1Burst () {
+    if (controller.right.isPressed()) {
+        p1.vx += 50
+        p1.startEffect(effects.fire, 200)
+        pause(200)
+        p1.vx += -30
+    }
+    if (controller.left.isPressed()) {
+        p1.vx += -50
+        p1.startEffect(effects.fire, 200)
+        pause(200)
+        p1.vx += 30
+    }
+    if (controller.down.isPressed()) {
+        p1.vy += 50
+        p1.startEffect(effects.fire, 200)
+        pause(200)
+        p1.vy += -30
+    }
+    if (controller.up.isPressed()) {
+        p1.vy += -50
+        p1.startEffect(effects.fire, 200)
+        pause(200)
+        p1.vy += 30
+    }
+}
+function p2Burst () {
+    if (controller.player2.isPressed(ControllerButton.Right)) {
+        p2.vx += 50
+        p2.startEffect(effects.fire, 200)
+        pause(200)
+        p2.vx += -30
+    }
+    if (controller.player2.isPressed(ControllerButton.Left)) {
+        p2.vx += -50
+        p2.startEffect(effects.fire, 200)
+        pause(200)
+        p2.vx += 30
+    }
+    if (controller.player2.isPressed(ControllerButton.Down)) {
+        p2.vy += 50
+        p2.startEffect(effects.fire, 200)
+        pause(200)
+        p2.vy += -30
+    }
+    if (controller.player2.isPressed(ControllerButton.Up)) {
+        p2.vy += -50
+        p2.startEffect(effects.fire, 200)
+        pause(200)
+        p2.vy += 30
+    }
+}
+function arrowControls () {
+    if (controller.right.isPressed()) {
+        p1.vx += 1
+    }
+    if (controller.left.isPressed()) {
+        p1.vx += -1
+    }
+    if (controller.up.isPressed()) {
+        p1.vy += -1
+    }
+    if (controller.down.isPressed()) {
+        p1.vy += 1
+    }
+    if (controller.player2.isPressed(ControllerButton.Right)) {
+        p2.vx += 1
+    }
+    if (controller.player2.isPressed(ControllerButton.Left)) {
+        p2.vx += -1
+    }
+    if (controller.player2.isPressed(ControllerButton.Up)) {
+        p2.vy += -1
+    }
+    if (controller.player2.isPressed(ControllerButton.Down)) {
+        p2.vy += 1
+    }
+}
+controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    if (_2burst == 0) {
+        _2burst = 1
+        p2Burst()
+        music.playTone(262, music.beat(BeatFraction.Sixteenth))
+        music.playTone(294, music.beat(BeatFraction.Sixteenth))
+        music.playTone(330, music.beat(BeatFraction.Sixteenth))
+        music.playTone(349, music.beat(BeatFraction.Sixteenth))
+        pause(2000)
+        _2burst = 0
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+    } else {
+        music.playTone(165, music.beat(BeatFraction.Quarter))
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (burst == 0) {
+        burst = 1
+        p1Burst()
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        music.playTone(262, music.beat(BeatFraction.Sixteenth))
+        music.playTone(294, music.beat(BeatFraction.Sixteenth))
+        music.playTone(330, music.beat(BeatFraction.Sixteenth))
+        pause(2000)
+        burst = 0
+        music.playTone(349, music.beat(BeatFraction.Sixteenth))
+    } else {
+        music.playTone(165, music.beat(BeatFraction.Quarter))
+    }
+})
+info.onCountdownEnd(function () {
+    tiles.setTilemap(tiles.createTilemap(
+            hex`100010000202020b0d0d0d0d0d0d0d0d0c0202020202020b0d0d0d0d0d0d0d0d0c0202020202020b0d0d0d0d0d0d0d0d0c0202020202020b0d0d070d0d060d0d0c0202020202020b0d0d0d0d0d0d0d0d0c020202020202080a0a0a0a0a0a0a0a0902020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,sprites.builtin.crowd0,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.collectibleBlueCrystal,myTiles.tile3,myTiles.tile4,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterWest0,sprites.dungeon.darkGroundCenter,myTiles.tile5,myTiles.tile6],
+            TileScale.Sixteen
+        ))
+    pause(100)
+    game.showLongText("And the Winner is...", DialogLayout.Bottom)
+    if (info.score() == info.player2.score()) {
+        game.showLongText("No one!", DialogLayout.Bottom)
+        game.over(false)
+    } else {
+        if (info.score() > info.player2.score()) {
+            tiles.setTileAt(tiles.getTileLocation(9, 3), myTiles.tile5)
+            game.showLongText("Player 1!", DialogLayout.Bottom)
+            game.over(true, effects.confetti)
+        } else {
+            tiles.setTileAt(tiles.getTileLocation(6, 3), myTiles.tile6)
+            game.showLongText("Player 2!", DialogLayout.Bottom)
+            game.over(true, effects.confetti)
+        }
+    }
+})
+let p2vy = 0
+let p2vx = 0
+let p1vy = 0
+let p1vx = 0
+let _2burst = 0
+let burst = 0
+let p2: Sprite = null
+let p1: Sprite = null
+tiles.setTilemap(tiles.createTilemap(
+            hex`100010000b0d0d0d0d0d0d0d0d0c0202020202020b0d0d0d0d0d0d0d0d0c0202020202020b0d0d070d0d060d0d0c0202020202020b0d0d0d0d0d0d0d0d0c020202020202080a0a0a0a0a0a0a0a090202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,sprites.builtin.crowd0,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.collectibleBlueCrystal,myTiles.tile3,myTiles.tile4,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterWest0,sprites.dungeon.darkGroundCenter,myTiles.tile5,myTiles.tile6],
+            TileScale.Sixteen
+        ))
+game.showLongText("2 Player Arena!", DialogLayout.Bottom)
+game.showLongText("Use 'WASD' and 'Q' for Player 1. Use 'IJKL' and 'O' for Player 2.", DialogLayout.Bottom)
+game.showLongText("Accelerate into your foe in order to win! 'Q/O' is for boosting!", DialogLayout.Bottom)
+tiles.setTilemap(tiles.createTilemap(
+            hex`10001000231b1b1b1b241b1b1b1b241b1b1b1b221a13131313131313131313131313131c1a13030909090909090909090902131c1a13061718121212121212171808131c1a13061516121212121212151608131c271306121212121212121212120813251a13061212122d1e2c1e12121208131c1a13061212121e2a2a2d12121208131c1a13061212122d2a2a2c12121208131c1a13061212122b2c2b2d12121208131c271306121212121212121212120813251a13061718121212121212171808131c1a13061516121212121212151608131c1a13040707070707070707070701131c1a13131313131313131313131313131c20191919192619191919261919191921`,
+            img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . 2 2 . . . . . . 2 2 . . 2 
+2 . . 2 2 . . . . . . 2 2 . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . 2 2 . . . . . . 2 2 . . 2 
+2 . . 2 2 . . . . . . 2 2 . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`,
+            [myTiles.tile0,sprites.dungeon.purpleInnerSouthEast,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleInnerSouthWest,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterEast1,sprites.dungeon.purpleOuterNorth0,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterSouth1,sprites.dungeon.darkGroundSouthWest0,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.darkGroundSouthEast0,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundCenter,myTiles.tile1,sprites.dungeon.buttonTeal,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterEast0,sprites.dungeon.floorLight2,sprites.dungeon.floorDark0,myTiles.tile2,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterSouth2,sprites.dungeon.greenOuterWest2,sprites.dungeon.floorDark2,sprites.dungeon.floorDark5,sprites.dungeon.floorDarkDiamond,sprites.dungeon.floorDark3,sprites.dungeon.floorDark1,sprites.dungeon.floorDark4,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6],
+            TileScale.Sixteen
+        ))
+p1 = sprites.create(img`
+. . . . . . f f f f . . . . . . 
+. . . . f f 2 2 2 2 f f . . . . 
+. . f f 2 2 2 1 1 2 2 2 f f . . 
+. . f 2 2 2 2 1 1 2 2 2 2 f . . 
+. f 2 2 2 1 1 1 1 2 2 2 2 2 f . 
+. f 2 2 2 1 1 1 1 2 2 2 2 2 f . 
+f 2 2 2 2 2 2 1 1 2 2 2 2 2 2 f 
+f 2 2 2 2 2 2 1 1 2 2 2 2 2 2 f 
+f 2 2 2 2 2 2 1 1 2 2 2 2 2 2 f 
+f 2 2 2 2 2 2 1 1 2 2 2 2 2 2 f 
+. f 2 2 2 2 2 1 1 2 2 2 2 2 f . 
+. f 2 2 2 1 1 1 1 1 1 2 2 2 f . 
+. . f 2 2 1 1 1 1 1 1 2 2 f . . 
+. . f f 2 2 2 2 2 2 2 2 f f . . 
+. . . . f f 2 2 2 2 f f . . . . 
+. . . . . . f f f f . . . . . . 
+`, SpriteKind.Player)
+p2 = sprites.create(img`
+. . . . . . f f f f . . . . . . 
+. . . . f f 8 8 8 8 f f . . . . 
+. . f f 8 8 8 8 8 8 8 8 f f . . 
+. . f 8 8 1 1 1 1 1 1 8 8 f . . 
+. f 8 8 1 1 1 1 1 1 1 1 8 8 f . 
+. f 8 8 8 8 8 8 8 8 1 1 8 8 f . 
+f 8 8 8 8 8 8 8 8 8 1 1 8 8 8 f 
+f 8 8 8 8 1 1 1 1 1 1 1 8 8 8 f 
+f 8 8 8 1 1 1 1 1 1 1 8 8 8 8 f 
+f 8 8 8 1 1 8 8 8 8 8 8 8 8 8 f 
+. f 8 8 1 1 8 8 8 8 8 8 8 8 f . 
+. f 8 8 1 1 1 1 1 1 1 1 8 8 f . 
+. . f 8 8 1 1 1 1 1 1 8 8 f . . 
+. . f f 8 8 8 8 8 8 8 8 f f . . 
+. . . . f f 8 8 8 8 f f . . . . 
+. . . . . . f f f f . . . . . . 
+`, SpriteKind.Player)
+tiles.placeOnTile(p1, tiles.getTileLocation(6, 6))
+tiles.placeOnTile(p2, tiles.getTileLocation(9, 9))
+p1.setFlag(SpriteFlag.StayInScreen, true)
+p2.setFlag(SpriteFlag.StayInScreen, true)
+burst = 0
+_2burst = 0
+info.setScore(0)
+info.player2.setScore(0)
+info.startCountdown(120)
+game.showLongText("Press 'A' to Start", DialogLayout.Bottom)
+forever(function () {
+    arrowControls()
+    scene.centerCameraAt((p1.x + p2.x) / 2, (p1.y + p2.y) / 2)
+    if (p1.overlapsWith(p2)) {
+        p1vx = p1.vx
+        p1vy = p1.vy
+        p2vx = p2.vx
+        p2vy = p2.vy
+        p1.vx = p2vx
+        p1.vy = p2vy
+        p2.vx = p1vx
+        p2.vy = p1vy
+    }
+})
